4590ba291008e869b6c4a27ee13c685fa1ea0e2f
EhsaN Karimi <ehsan.karimmi@gmail.com>
2024-03-12 04:55:05 +0330

    add: navbar component and all routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Cryptocurrencies from "./pages/Cryptocurrencies";
+import Exchanges from "./pages/Exchanges";
+import News from "./pages/News";
 
 function App() {
-    return <div className="h-2 text-red-500 font-semibold">App</div>;
+    return (
+        <BrowserRouter>
+            {/* main pages structure */}
+            <main className=" h-screen grid grid-cols-12">
+                {/* navbar component -> left side */}
+                <Navbar />
+                {/* pages and footer -> right side */}
+                <div className="col-span-10">
+                    {/* pages routes */}
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+                        <Route path="/exchanges" element={<Exchanges />} />
+                        <Route path="/news" element={<News />} />
+                    </Routes>
+                    {/* footer component */}
+                    <Footer />
+                </div>
+            </main>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import logo from "../assets/images/crypto-hunter-logo.png";
+import { Link } from "react-router-dom";
+import { navbarLinks } from "../utils/NavbarLinks";
+
+function Navbar() {
+    return (
+        <nav className="col-span-2 bg-zinc-900  rounded-r-lg">
+            {/* logo */}
+            <Link to={"/"}>
+                <div className="flex justify-start items-center gap-2 p-5">
+                    <img src={logo} alt="Logo" className="w-8 h-8" />
+                    <h3 className="uppercase font-bold text-xl text-yellow-400">
+                        Crypto Hunter
+                    </h3>
+                </div>
+            </Link>
+            {/* links */}
+            <ul className="my-20 mr-1">
+                {navbarLinks.map((link) => {
+                    return (
+                        <Link key={link.title} to={link.to}>
+                            <li className="flex justify-start items-center gap-3 my-3 p-3 rounded-r-lg border-l-4 border-l-transparent font-medium text-white hover:bg-yellow-400 hover:text-zinc-900 transition-all duration-300">
+                                <span>{link.icon}</span>
+                                <span>{link.title}</span>
+                            </li>
+                        </Link>
+                    );
+                })}
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
