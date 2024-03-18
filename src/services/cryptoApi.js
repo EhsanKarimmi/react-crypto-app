90b574bc95242240cedf7506830c2010aa917983
EhsaN Karimi <ehsan.karimmi@gmail.com>
2024-03-18 04:17:48 +0330

    fix: .env and vite conflict

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,11 +2,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // req headers from rapidApi.
 const cryptoApiHeaders = {
-    "X-RapidAPI-Key": process.env.RAPID_API_SECRET_KEY,
-    "X-RapidAPI-Host": process.env.RAPID_API_HOST,
+    "X-RapidAPI-Key": import.meta.env.VITE_REACT_RAPID_API_SECRET_KEY,
+    "X-RapidAPI-Host": import.meta.env.VITE_REACT_RAPID_API_HOST,
 };
 // base url for request.
-const baseUrl = process.env.CRYPTO_API_BASE_URL;
+const baseUrl = import.meta.env.VITE_REACT_CRYPTO_API_BASE_URL;
 
 // our request to get data.
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    // when we don't want use import.meta.env => uncomment this line.
+    // define: { "process.env": {} },
 });
